0397e8903799323f8fef4b5af9007938aa1ef47c
Mike Hausmann <hausmannmike@yahoo.com>
2026-02-22 14:03:56 +0100

    in main, added calculation for correlation

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ msft = get_daily_close_prices("MSFT", start, end)
 prices = pd.concat([aapl, msft], axis=1)
 prices.columns = ["AAPL", "MSFT"]
 
+# for each timestamp (day) we do: return_t = [price_t / price_(t-1)] - 1, this results in pecentage movements...
+returns = prices.pct_change().dropna()
+
+corr = returns["AAPL"].corr(returns["MSFT"])
+
 # print(prices)
-print(prices)
+print(returns)
 print()
+print("Correltion between AAPL and MSFT: ", corr)
