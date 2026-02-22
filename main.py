e08cd2fac7af60d89e76db7ff9dc8c07732d9107
Mike Hausmann <hausmannmike@yahoo.com>
2026-02-22 14:17:30 +0100

    scaled it, to support a list of tickers instead of just two tickers

--- main.py
+++ main.py
@@ -5,18 +5,22 @@ from engine import get_daily_close_prices
 start  = datetime(2023, 1, 1, tzinfo=timezone.utc)
 end  = datetime(2024, 1, 1, tzinfo=timezone.utc)
 
-aapl = get_daily_close_prices("AAPL", start, end)
-msft = get_daily_close_prices("MSFT", start, end)
+tickers = ["AAPL", "MSFT", "NVDA", "AMZN", "META", "XOM", "GLD"]
 
-prices = pd.concat([aapl, msft], axis=1)
-prices.columns = ["AAPL", "MSFT"]
+prices = pd.DataFrame()
+
+for ticker in tickers:
+    closes = get_daily_close_prices(ticker, start, end)
+    closes.name = ticker
+    prices = pd.concat([prices, closes], axis=1)
 
 # for each timestamp (day) we do: return_t = [price_t / price_(t-1)] - 1, this results in pecentage movements...
 returns = prices.pct_change().dropna()
 
-corr = returns["AAPL"].corr(returns["MSFT"])
+corr = returns.corr()
 
 # print(prices)
 print(returns)
 print()
-print("Correltion between AAPL and MSFT: ", corr)
+print("Correltion Matrix:")
+print(corr)
